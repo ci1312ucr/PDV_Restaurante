1826430d2253d0a1af5aabb5560fa96251cb7c6c
Roberto <Roberto@DESKTOP-07K9S1N>
2017-07-14 23:39:35 -0600

    Obketos de factura, plato e ingrediente

--- PDVRestaurante/PDVRestaurante/BaseDatos/TablaPlato.cs
+++ PDVRestaurante/PDVRestaurante/BaseDatos/TablaPlato.cs
@@ -4,6 +4,9 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PDVRestaurante.Ayudantes;
+using PDVRestaurante.Constantes;
+using PDVRestaurante.Objetos;
 
 namespace PDVRestaurante.BaseDatos
 {
@@ -27,6 +30,48 @@ namespace PDVRestaurante.BaseDatos
         private static string LlavePrincipal()
         {
             return "IdPlato";
+        }
+
+        public static bool InsertarPlato(params object[] parametros)
+        {
+            if (parametros.Count() == Columnas().Split('|').Count())
+            {
+                InterpreteSQL.Insertar(ConnectionString(), Tabla(), Columnas(), parametros);
+            }
+            return true;
+        }
+
+        public static bool ModificarPlato(int id, params object[] parametros)
+        {
+            if (parametros.Count() == Columnas().Split('|').Count())
+            {
+                InterpreteSQL.Modificar(ConnectionString(), Tabla(), Columnas(), LlavePrincipal(), id.ToString(), parametros);
+            }
+            return true;
+        }
+
+        public static Plato ObtenerPlato(int id)
+        {
+            Plato plato = null;
+            var dataSet = InterpreteSQL.Obtener(ConnectionString(), Tabla(), Columnas(), "c.IdPlato", id.ToString(), CriterioSQL.IgualA);
+
+            if (dataSet.Tables.Count > 0)
+            {
+                plato = Convertidor.DataSetAObjecto<Plato>(dataSet).FirstOrDefault();
+            }
+            return plato;
+        }
+
+        public static List<Plato> ObtenerPlatos(string columnasFiltro = null, string valoresFiltro = null, string criteriosFiltro = null)
+        {
+            var platos = new List<Plato>();
+            var dataSet = InterpreteSQL.Obtener(ConnectionString(), Tabla(), Columnas(), columnasFiltro, valoresFiltro, criteriosFiltro);
+
+            if (dataSet.Tables.Count > 0)
+            {
+                platos = Convertidor.DataSetAObjecto<Plato>(dataSet);
+            }
+            return platos;
         }
     }
 }
--- PDVRestaurante/PDVRestaurante/Pantallas/Consultas/ConsultaFacturas.cs
+++ PDVRestaurante/PDVRestaurante/Pantallas/Consultas/ConsultaFacturas.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using PDVRestaurante.Ayudantes;
+using PDVRestaurante.BaseDatos;
+using PDVRestaurante.Constantes;
+using PDVRestaurante.Objetos;
+using PDVRestaurante.Pantallas.Mantenimiento.Empleados;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +17,15 @@ namespace PDVRestaurante.Pantallas.Consultas
 {
     public partial class ConsultaFacturas : Form
     {
+        private List<Propiedad> _columnas;
+        private List<Factura> _clientes;
+        private string _ordenActual = "IdIngrediente";
+
         public ConsultaFacturas()
         {
             InitializeComponent();
+
+            InicializaListView();
         }
     }
 }
